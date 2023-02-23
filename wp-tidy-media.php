404183dcaea193642e22e5e5c7dad2dc5be83805
robertandrews <mail@robertandrews.co.uk>
2023-02-23 18:41:32 +0000

    Format settings table

--- wp-tidy-media.php
+++ wp-tidy-media.php
@@ -41,7 +41,7 @@ function tidy_media_organizer_create_table()
 
 
 // Display admin page
-add_action('foo', 'tidy_media_organizer_admin_page');
+add_action('admin_menu', 'tidy_media_organizer_admin_page');
 function tidy_media_organizer_admin_page()
 {
     add_options_page(
@@ -52,6 +52,14 @@ function tidy_media_organizer_admin_page()
         'tidy_media_organizer_options_page'
     );
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -82,21 +90,30 @@ if (isset($_POST['tidy_media_organizer_save'])) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'tidy_media_organizer';
 
-    // Update or create settings
-    $settings = array(
-        array('setting_name' => 'organize_by_post_type', 'setting_value' => isset($_POST['organize_by_post_type']) ? 1 : 0),
-        array('setting_name' => 'organize_by_taxonomy', 'setting_value' => isset($_POST['organize_by_taxonomy']) ? sanitize_text_field($_POST['organize_by_taxonomy']) : ''),
-    );
-    foreach ($settings as $setting) {
-        $existing_row = $wpdb->get_row("SELECT * FROM $table_name WHERE setting_name = '{$setting['setting_name']}'");
-        if ($existing_row) {
-            $wpdb->query("UPDATE $table_name SET setting_value = '{$setting['setting_value']}' WHERE setting_name = '{$setting['setting_name']}'");
-        } else {
-            $wpdb->insert($table_name, $setting);
+// Update or create settings
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name) {
+        $settings = array(
+            array('setting_name' => 'organize_by_post_type', 'setting_value' => isset($_POST['organize_by_post_type']) ? 1 : 0),
+            array('setting_name' => 'organize_by_taxonomy', 'setting_value' => isset($_POST['organize_by_taxonomy']) ? sanitize_text_field($_POST['organize_by_taxonomy']) : ''),
+        );
+        foreach ($settings as $setting) {
+            $existing_row = $wpdb->get_row("SELECT * FROM $table_name WHERE setting_name = '{$setting['setting_name']}'");
+            if ($existing_row) {
+                $wpdb->query("UPDATE $table_name SET setting_value = '{$setting['setting_value']}' WHERE setting_name = '{$setting['setting_name']}'");
+            } else {
+                $wpdb->insert($table_name, $setting);
+            }
         }
+        echo '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
+    } else {
+        // Show an error message
+        echo '<div class="notice notice-error"><p><strong>Save failed</strong>: <code>' . $table_name . '</code> not found in database. Cannot store settings. Try reactivating the plugin.</p></div>';
     }
-    echo '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
+
 }
+
+
+
 
 
 
@@ -120,40 +137,78 @@ if (isset($_POST['tidy_media_organizer_save'])) {
 
 
 
+
+
+
+
+
+
     
     // Output form HTML
     ?>
     <div class="wrap">
         <h1>Tidy Media Organizer</h1>
+
+
+
+
         <form method="post">
             <table class="form-table">
                 <tbody>
+
+
+
+
                     <tr>
                         <th scope="row">
                             <label for="organize_by_post_type">Organize by post type?</label>
                         </th>
                         <td>
                             <input type="checkbox" name="organize_by_post_type" id="organize_by_post_type" value="1" <?php checked($organize_by_post_type, 1);?>>
+                                <?php
+                                // Show post types
+                                $args = array(
+                                    'public' => true,
+                                    '_builtin' => false, // exclude default post types
+                                );
+                                $post_types = get_post_types($args);
+                                // add back default post types 'post' and 'page'
+                                array_push($post_types, 'post', 'page');
+                                echo '('.implode(', ', array_map(function ($post_type) {
+                                    return '<code>' . $post_type . '</code>';
+                                }, $post_types)).')';
+                                ?>
                         </td>
                     </tr>
+
+
+                    
                     <tr>
                         <th scope="row">
                             <label>Organize by taxonomy:</label>
                         </th>
                         <td>
-                            <?php
-                            $taxonomies = get_taxonomies();
-                                foreach ($taxonomies as $taxonomy) {
-                                ?>
-                                    <label>
-                                        <input type="radio" name="organize_by_taxonomy" value="<?php echo esc_attr($taxonomy); ?>" <?php checked($organize_by_taxonomy, $taxonomy);?>>
-                                        <?php echo esc_html($taxonomy); ?>
-                                    </label>
-                                    <br>
-                                    <?php
-                                }
-                                ?>
+<?php
+                            $taxonomies = get_taxonomies(array('public' => true));
+                            foreach ($taxonomies as $taxonomy) {
+                            ?>
+                                <label style="margin: 0.35em 0 0.5em!important; display: inline-block;">
+                                    <input type="radio" name="organize_by_taxonomy" value="<?php echo esc_attr($taxonomy); ?>" <?php checked($organize_by_taxonomy, $taxonomy);?>>
+                                    <code><?php echo esc_html($taxonomy); ?></code>
+                                </label>
+                                <br>
+                                <?php
+                            }
+                            ?>
+
                         </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">
+                            <label>Preview:</label>
+                        </th>
+                        <td><div id="planned-path"></div></td>
                     </tr>
                 </tbody>
             </table>
